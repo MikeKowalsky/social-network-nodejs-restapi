abc79f6e6e69bce5ffdeb81890b6ace7656a2b66
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-23 19:19:47 +0100

    added login with jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // const User = require("../models/user");
 const User = require("../models/user");
@@ -30,6 +31,44 @@ exports.signup = (req, res, next) => {
       res.status(201).json({ message: "User created!", userId: result._id })
     )
     .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        const error = new Error("User with taht email not found!");
+        error.statusCode = 404;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Password incorrect!");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        "secretword",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token, userId: loadedUser._id.toString() });
+    })
+    .catch(er => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
