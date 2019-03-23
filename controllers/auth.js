d31c27b3bb8f5f530dc368a0e0f390946d7a9425
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-23 18:46:03 +0100

    implemented signin functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
 
+// const User = require("../models/user");
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     const error = new Error("Validation failed");
     error.statusCode = 422;
     error.data = errors.array();
@@ -12,4 +14,25 @@ exports.signup = (req, res, next) => {
   }
 
   const { email, password, name } = req.body;
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const newUser = new User({
+        email,
+        password: hashedPassword,
+        name
+      });
+
+      return newUser.save();
+    })
+    .then(result =>
+      res.status(201).json({ message: "User created!", userId: result._id })
+    )
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
